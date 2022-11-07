7285d05ab23f13e9267684d2588a49902b2476b8
Salman Zafar <18salmanz@gmail.com>
2022-11-07 09:02:17 +0530

    Added update, delete and Refactored code structure

--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,22 @@
 import express from 'express';
-import { v4 as uuidv4 } from 'uuid';
+import {
+    createUser,
+    getUsers,
+    getUser,
+    deleteUser,
+    updateUser,
+} from '../controllers/users.js';
 
 const router = express.Router();
 
-const users = [];
+router.get('/', getUsers);
 
-router.get('/', (req, res) => {
-    res.send(users);
-});
+router.post('/', createUser);
 
-router.post('/', (req, res) => {
-    const user = req.body;
+router.get('/:id', getUser);
 
-    const userId = uuidv4();
+router.delete('/:id', deleteUser);
 
-    const userWithId = { ...user, id: userId };
-
-    users.push(userWithId);
-    res.send(`Post Routes has been done for ${user.firstName}`);
-});
-
-router.get('/:id', (req, res) => {
-    res.send('The GET ID Route');
-});
+router.patch('/:id', updateUser);
 
 export default router;
